eadea8475b488510e7ca5e3825c290ee51ed7fa7
Ricardo Sousa <r.sousa.888@gmail.com>
2024-09-16 14:07:43 +0100

    alteraçoes

--- Intervalos/settings.py
+++ Intervalos/settings.py
@@ -9,9 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5*0#o3bmha(_5koh#@@b(-s6fp2g6)=rofv4zuu9jf1+fu1dqq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['rikb1g.pythonanywhere.com']
+#ALLOWED_HOSTS = ['rikb1g.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
